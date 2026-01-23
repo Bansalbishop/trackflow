70e119cc674bdaf40b25a284e227987d3a2ac7d8
bb <bishopbansal2005@gmail.com>
2026-01-23 09:23:20 +0530

    bug fixed

--- trackflow/src/components/LandingFooter.jsx
+++ trackflow/src/components/LandingFooter.jsx
@@ -52,9 +52,9 @@ const LandingFooter = () => {
       <hr className="border-gray-300 my-6"></hr>
 
       <div className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap">
-        <p>Copyright 2025 - TaskFlow - All Right Reserved. </p>
+        <p>Copyright 2026 - TaskFlow - All Right Reserved. </p>
 
-        <div className="flex items-center justify-between gap-4 cursor-pointer" >
+        <div className="flex items-center justify-between gap-4 cursor-pointer">
           <img src={assets.facebook_icon} alt="" />
           <img src={assets.twitter_icon} alt="" />
           <img src={assets.instagram_icon} alt="" />
