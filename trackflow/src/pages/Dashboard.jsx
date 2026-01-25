5db2dc964480d26687b8b40a9b9588f4e16126ef
bb <bishopbansal2005@gmail.com>
2026-01-26 01:05:28 +0530

    style: format code for better readability in Dashboard component

--- trackflow/src/pages/Dashboard.jsx
+++ trackflow/src/pages/Dashboard.jsx
@@ -5,17 +5,13 @@ import assets from "../assets/assets";
 import Dashlist from "../components/Dashlist";
 import { useDashboard } from "../contexts/Dashboard.context";
 const Dashboard = () => {
-  const{tasks,projects}=useDashboard();
-
-
-
-
+  const { tasks, projects } = useDashboard();
 
   return (
     <>
       <div className="relative bg-blue-400/10">
         <DashNavbar />
-        <div >
+        <div>
           <div className="flex mx-5 my-10 rounded-2xl p-4 sm:mx-5 md:mx-20 lg:mx-40 text-black bg-white gap-10 sm:gap-0 flex-col shadow-lg sm:flex-row justify-between items-center">
             <div className="flex flex-col items-center gap-1">
               <h2 className="font-extrabold text-5xl sm:text-3xl md:text-4xl  ">
@@ -39,11 +35,9 @@ const Dashboard = () => {
             </div>
           </div>
 
-
           <div className="flex mx-5 my-10 rounded-2xl p-4 sm:mx-5 md:mx-20 lg:mx-40 text-black bg-white gap-10 sm:gap-0  flex-col shadow-lg sm:flex-row justify-between  items-center md:1/3 lg:w-1/4">
-            <Dashlist/>
+            <Dashlist />
           </div>
-
         </div>
       </div>
     </>
